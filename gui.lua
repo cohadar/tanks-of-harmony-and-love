09957762fcd0b6479f854b6cbc056a6484007fa3
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-05-07 09:41:18 +0200

    minimum viable gui

--- gui.lua
+++ gui.lua
@@ -2,12 +2,16 @@
 local m_gui = {}
 
 gui = require "libs.quickie"
+m_text = require "text"
 
 local gui_on = true
 
 local start_menu = true
 local create_menu = false
 local join_menu = false
+
+local server_address = { text = "127.0.0.1" }
+local server_port    = { text = "12345" }
 
 -------------------------------------------------------------------------------
 function m_gui.reset_menus()
@@ -22,10 +26,14 @@ function m_gui.update( dt )
 		return
 	end
 
-	gui.group.push{ grow = "down", pos = { 5, 150 } }
+	gui.group.push{ grow = "down", pos = { 10, 10 } }
 
 	if start_menu then
  		m_gui.start_menu( dt )
+ 	elseif create_menu then
+ 		m_gui.create_menu( dt )
+ 	elseif join_menu then
+ 		m_gui.join_menu( dt )
 	end
 
     gui.group.pop{}    
@@ -41,12 +49,37 @@ function m_gui.start_menu( dt )
 	if gui.Button{id = "join", text = "Join Game"} then
 		start_menu = false
 		join_menu = true
-        gui_on = false
     end   
 
 	if gui.Button{id = "exit", text = "Exit"} then
 		love.event.quit()
     end      
+end
+
+-------------------------------------------------------------------------------
+function m_gui.create_menu( dt )
+    gui.group{grow = "down", function()
+        gui.Input{info = server_address, size = { 200 } }
+        gui.Input{info = server_port, size = { 200 } }
+        if gui.Button{id = "start_server", text = "Start Server"} then
+			start_menu = true
+			create_menu = false
+			m_text.print("starting server", server_address.text .. ":" .. server_port.text )
+    	end
+    end}	
+end
+
+-------------------------------------------------------------------------------
+function m_gui.join_menu( dt )
+    gui.group{grow = "down", function()
+        gui.Input{info = server_address, size = { 200 } }
+        gui.Input{info = server_port, size = { 200 } }
+        if gui.Button{id = "join_server", text = "Join Server"} then
+			start_menu = true
+			create_menu = false
+			m_text.print("joining server", server_address.text .. ":" .. server_port.text )
+    	end
+    end}	
 end
 
 -------------------------------------------------------------------------------
