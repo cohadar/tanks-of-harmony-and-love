667a37b522d5000012f6806c9f1f5eeac0f5fae1
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-05-09 08:55:57 +0200

    ticker ref

--- ticker.lua
+++ ticker.lua
@@ -2,7 +2,7 @@
 -- time module firing tick events on even power two edges of os.clock()
 -------------------------------------------------------------------------------
 --- @module ticker
-local m_ticker = {}
+local ticker = {}
 
 local t = 0
 local DT = 1 / 32 -- MUST be power of 2 
@@ -10,7 +10,7 @@ local DT = 1 / 32 -- MUST be power of 2
 -------------------------------------------------------------------------------
 -- mark is time in seconds
 -------------------------------------------------------------------------------
-function m_ticker.init( mark )
+function ticker.init( mark )
 	t = math.floor( mark ) 
 	while t < mark do
 		t = t + DT
@@ -20,7 +20,7 @@ end
 -------------------------------------------------------------------------------
 -- returns true if time for another tick
 -------------------------------------------------------------------------------
-function m_ticker.tick( mark, p )
+function ticker.tick( mark, p )
 	if t + DT < mark then
 		if p then
 			print(mark - t)
@@ -32,4 +32,4 @@ function m_ticker.tick( mark, p )
 end
 
 -------------------------------------------------------------------------------
-return m_ticker
+return ticker
