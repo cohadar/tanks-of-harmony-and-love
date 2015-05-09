2f04868c585158b0c2714ef9883254f826f305c6
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-05-09 09:01:36 +0200

    utils ref

--- utils.lua
+++ utils.lua
@@ -1,41 +1,35 @@
 --- @module utils
-local m_utils = {}
+local utils = {}
 
 local smallfolk = require "libs.smallfolk"
 
 -------------------------------------------------------------------------------
-function m_utils.deepcopy( orig )
-    local copy = orig
-    if type(orig) == 'table' then
-        copy = {}
-        for orig_key, orig_value in next, orig, nil do
-            copy[ m_utils.deepcopy(orig_key) ] = m_utils.deepcopy( orig_value )
-        end
-        --setmetatable(copy, m_utils.deepcopy(getmetatable(orig)))
-    end
-    return copy
-end
+-- facade over packing methods so we can change implementations
+-------------------------------------------------------------------------------
+utils.pack = smallfolk.dumps
+utils.unpack = smallfolk.loads
 
 -------------------------------------------------------------------------------
-function m_utils.round( num, idp )
-  local mult = 10 ^ ( idp or 0 )
-  return math.floor( num * mult + 0.5 ) / mult
+-- data-only deepcopy
+-------------------------------------------------------------------------------
+function utils.deepcopy( orig )
+    local copy = orig
+    if type( orig ) == 'table' then
+        copy = {}
+        for key, value in next, orig, nil do
+            copy[ utils.deepcopy( key ) ] = utils.deepcopy( value )
+        end
+    end
+    return copy
 end
 
 -------------------------------------------------------------------------------
 -- we round up the angles because we don't really need all extra precision
 -- and rounded angles are much easier synchronized in multiplayer
 -------------------------------------------------------------------------------
-function m_utils.round_angle( num )
+function utils.round_angle( num )
   return math.floor( num * 0x10000 + 0.5 ) / 0x10000
 end
 
 -------------------------------------------------------------------------------
-m_utils.pack = smallfolk.dumps
-
--------------------------------------------------------------------------------
-m_utils.unpack = smallfolk.loads
-
-
--------------------------------------------------------------------------------
-return m_utils
+return utils
