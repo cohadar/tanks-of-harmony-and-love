9fcfefb16507b6115b970483c3165177f5150f8a
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-05-09 08:26:02 +0200

    server ref

--- server.lua
+++ server.lua
@@ -1,97 +1,97 @@
 -------------------------------------------------------------------------------
--- embedded game server for "Tanks of Harmony and Love"
+-- embedded game server for "_tanks of Harmony and Love"
 -- can NOT be run as standalone Lua app!
 -- TODO: figure out how to call this in love2d console mode!
 -------------------------------------------------------------------------------
 require "enet"
-m_utils = require "utils"
-m_tank = require "tank"
-m_text = require "text"
-m_ticker = require "ticker"
 require "love.timer"
+utils = require "utils"
+tank = require "tank"
+text = require "text"
+ticker = require "ticker"
 
-local tanks = {}
-local tank_commands = {}
+local _tanks = {}
+local _tankCommands = {}
 
 -------------------------------------------------------------------------------
-local function on_connect( event ) 
+local function onConnect( event ) 
     local index = event.peer:index()
-    m_text.print( "connect", index, os.time() )
-    local gram = m_utils.pack{ type = "index", index = index }
-    tanks[ index ] = m_tank.new()
-    tank_commands[ index ] = m_tank.newCommand()
-    event.peer:send( gram, 0,  "unsequenced" )
+    text.print( "connect", index, os.time() )
+    _tanks[ index ] = tank.new()
+    _tankCommands[ index ] = tank.newCommand()
+    local datagram = utils.pack{ type = "index", index = index }
+    event.peer:send( datagram, 0,  "unsequenced" )
 end
 
 -------------------------------------------------------------------------------
-local function on_disconnect( host, event )
-    m_text.print( "disconnect", event.peer:index() )
-    tanks[ event.peer:index() ] = nil
-    local gram = m_utils.pack{ type = "player_gone", index = event.peer:index() } 
+local function onDisconnect( host, event )
+    local index = event.peer:index()
+    text.print( "disconnect", index )
+    _tanks[ index ] = nil
+    local gram = utils.pack{ type = "player_gone", index = index } 
     host:broadcast( gram )
 end
 
 -------------------------------------------------------------------------------
-local function on_receive( event, server_tick )
-    t = os.time()
-    msg = m_utils.unpack( event.data )
+local function onReceive( event, server_tick )
+    msg = utils.unpack( event.data )
     if msg.type == "tank_command" then
-        tank_commands[ event.peer:index() ] = msg.tank_command
+        _tankCommands[ event.peer:index() ] = msg.tank_command
     else
-        m_text.print( "ERROR: unknown msg.type", msg.type, event.data )
+        text.print( "ERROR: unknown msg.type", msg.type, event.data )
     end
 end
 
 -------------------------------------------------------------------------------
-local function on_update()
-    for index, tank in pairs( tanks ) do 
-        tank_command = tank_commands[ index ]
-        m_tank.update( tank, tank_command )
+local function onUpdate()
+    for index, tnk in pairs( _tanks ) do 
+        tank_command = _tankCommands[ index ]
+        tank.update( tnk, tank_command )
         tank_command.client_tick = tank_command.client_tick + 1
     end    
 end
 
 -------------------------------------------------------------------------------
-function on_broadcast( host )
-    for index, tank in pairs( tanks ) do 
-        local gram = m_utils.pack { 
+local function onBroadcast( host )
+    for index, tnk in pairs( _tanks ) do 
+        local gram = utils.pack { 
             type = "tank", 
             index = index, 
-            tank = tank, 
-            client_tick = tank_commands[ index ].client_tick - 1
+            tank = tnk, 
+            client_tick = _tankCommands[ index ].client_tick - 1
         } 
         host:broadcast( gram, 0, "unsequenced" ) 
     end
 end
 
 -------------------------------------------------------------------------------
-local function start_server( host, port )
+local function startServer( host, port )
     server_address = host .. ":" .. port
-    m_text.print( "starting server", server_address )
+    text.print( "starting server", server_address )
     local host = enet.host_create( server_address )
     local tick = 0
     local mark = love.timer.getTime()
-    m_ticker.init( mark )
+    ticker.init( mark )
     while true do
         local event = host:service( 0 )
         if event then
             if event.type == "receive" then
-                on_receive( event, tick )
+                onReceive( event, tick )
             elseif event.type == "connect" then
-                on_connect( event )
+                onConnect( event )
             elseif event.type == "disconnect" then
-                on_disconnect( host, event )
+                onDisconnect( host, event )
             else
-                m_text.print( "ERROR: unknown event.type", event.type )
+                text.print( "ERROR: unknown event.type", event.type )
             end
         end
         mark = love.timer.getTime()
-        while m_ticker.tick( mark ) do
+        while ticker.tick( mark ) do
             tick = tick + 1
-            on_update()
+            onUpdate()
             -- we broadcast 3 times slower than we update
             if tick % 3 == 0 then
-                on_broadcast( host )
+                onBroadcast( host )
             end
         end
     end
@@ -102,7 +102,7 @@ end
 -------------------------------------------------------------------------------
 channel = love.thread.getChannel( "server_channel" )
 value = channel:demand()
-start_server( value.host, value.port )
+startServer( value.host, value.port )
 
 
 
